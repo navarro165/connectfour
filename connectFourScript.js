2b87d761d61426fcb5ad9ef0c300c066577f95b1
navarro165 <navarro165@gmail.com>
2018-10-05 15:54:06 -0500

    added extra cell logic to determine current player in a cell

--- connectFourScript.js
+++ connectFourScript.js
@@ -1,3 +1,13 @@
+var user = "user";
+var userColor = 'rgb(29, 3, 175)';
+
+var computer = "computer";
+var computerColor = 'rgb(175, 29, 3)';
+
+var board = $('table tr');
+
+
+
 $("td").click(getClickPosition);
 
 function getClickPosition() {
@@ -6,17 +16,34 @@ function getClickPosition() {
   var row = $this.closest('tr').index();
 
   console.log([col,row].join(','));
-
+  colorCellbyPlayer(col,row,user);
 }
 
 
 
-function colorCellbyPlayer(player){
+function colorCellbyPlayer(colPosition,rowPosition,player){
   if (player == "user") {
-
+      return board.eq(rowPosition).find('td').eq(colPosition).find('button').css('background-color',userColor);
   } else if (player == "computer") {
-
+      return board.eq(rowPosition).find('td').eq(colPosition).find('button').css('background-color',computerColor);
   }
+}
+
+
+function getCurrentCellPlayer(colPosition,rowPosition){
+  var currentColor = getCurrentColor(colPosition,rowPosition);
+  if (currentColor == userColor) {
+      return user;
+  } else if (currentColor == computerColor) {
+      return computer;
+  } else {
+      return null;
+  }
+}
+
+
+function getCurrentColor(colPosition,rowPosition){
+  return board.eq(rowPosition).find('td').eq(colPosition).find('button').css('background-color');
 }
 
 
