3ac493a75369e47f629121b682920e639fe66841
navarro165 <navarro165@gmail.com>
2018-10-18 02:02:27 -0500

    finished logic that that searches for a winning combination of chips

--- connectFourScript.js
+++ connectFourScript.js
@@ -1,16 +1,13 @@
 // (function(){
-
-let playerOneName = prompt('Player One: Please enter your name, you will be blue.');
-let playerTwoName = prompt('Player Two: Please enter your name, you will be red.');
-$("#playerOne").text(playerOneName +'score'); //findway to get strings working here
-$("#playerTwo").text(playerTwoName +'score');
-
-
 let playerOne = "playerOne";
 let playerOneColor = 'rgb(29, 3, 175)';
+let playerOneScore = 0;
+let playerOneName = prompt('Player One: Please enter your name, you will be blue.');
 
 let playerTwo = "playerTwo";
 let playerTwoColor = 'rgb(175, 29, 3)';
+let playerTwoScore = 0;
+let playerTwoName = prompt('Player Two: Please enter your name, you will be red.');
 
 let board = $('table tr');
 let defaultCellColor = 'rgb(240, 240, 240)';
@@ -18,17 +15,29 @@ let fallingChipDelay = 125;
 let chipFalling = false;
 let whosTurnisIt = playerOne;
 
+$("#playerOne").text(`${playerOneName}'s score is ${playerOneScore}`);
+$("#playerTwo").text(`${playerTwoName}'s score is ${playerTwoScore}`);
+$(".turnTag").text(`Your turn ${playerOneName}!`);
 
-$("td").click(userFillPosition);
+//the game starts with player 1 clicking on any of the cells
+//then each click is alternated between players 1 and 2
+$("td").on('click',function(){
+    var $this = $(this);
+    var position = getClickPosition($this);
+    var colPosition = position[0];
+    var rowPosition = position[1];
+    var bottomChip = checkBottom(colPosition);
+    displayWhosTurnisIt(whosTurnisIt);
+    chipFallOnClick(colPosition,rowPosition,bottomChip,whosTurnisIt); //once chip falls, winner would be determined and score updated
+});
 
-function userFillPosition() {
-  var $this = $(this);
-  var position = getClickPosition($this);
-  var colPosition = position[0];
-  var rowPosition = position[1];
-  var bottomChip = checkBottom(colPosition);
-  if (chipFalling === false && getCurrentColor(colPosition,rowPosition) === defaultCellColor) {
-      chipFallOnClick(colPosition,rowPosition,bottomChip,whosTurnisIt);
+function displayWhosTurnisIt(whosTurnisIt){
+  if (whosTurnisIt == playerTwo) {
+      $(".turnTag").text(`Your turn ${playerOneName}!`);
+  } else if (whosTurnisIt == playerOne) {
+      $(".turnTag").text(`Your turn ${playerTwoName}!`);
+  } else{
+      $(".turnTag").text(``);
   }
 }
 
@@ -54,15 +63,17 @@ function getCurrentColor(colPosition,rowPosition){
 }
 
 function chipFallOnClick(colPosition,rowPosition,bottomChip,player){
-  chipFalling = true;
-  if (player == "playerOne") {
-      chipFallByUser(colPosition,rowPosition,bottomChip,player);
-  } else if (player == "playerTwo") {
-      chipFallByUser(colPosition,rowPosition,bottomChip,player);
+  if (chipFalling === false && getCurrentColor(colPosition,rowPosition) === defaultCellColor) {
+    chipFalling = true;
+    if (player == "playerOne") {
+        chipFallByUserAndCheckWin(colPosition,rowPosition,bottomChip,player);
+    } else if (player == "playerTwo") {
+        chipFallByUserAndCheckWin(colPosition,rowPosition,bottomChip,player);
+    }
   }
 }
 
-function chipFallByUser(colPosition,rowPosition,bottomChip,player){
+function chipFallByUserAndCheckWin(colPosition,rowPosition,bottomChip,player){
   var timeDelay = 0;
   for (let rowFallingChip = rowPosition; rowFallingChip <= bottomChip; rowFallingChip++) {
     if (rowFallingChip != bottomChip) {
@@ -70,6 +81,7 @@ function chipFallByUser(colPosition,rowPosition,bottomChip,player){
     } else {
         setTimeout( () => {
           colorCellbyPlayer(colPosition,rowFallingChip,player);
+          checkForWinnerAndUpdateScore();
           chipFalling = false;
           if (whosTurnisIt == playerOne) {
               whosTurnisIt = playerTwo;
@@ -100,10 +112,65 @@ function uncolorCellbyPlayer(colPosition,rowPosition,player){
   }
 }
 
+function checkForWinnerAndUpdateScore(){
+  var hWin = checkHorizontalWin();
+  var vWin = checkVerticalWin();
+  var dWin = checkDiagonalWin();
+  if (hWin !== undefined) {
+      console.log("w");
+      return hWin;
+  } else if (vWin !== undefined) {
+      return vWin;
+  } else if (dWin !== undefined) {
+      return dWin;
+  }
+}
 
+function checkHorizontalWin(){
+  for (var row = 0; row < 6; row++) {
+    for (var col = 0; col < 4; col++) {
+      var matched =  checkForMatch(getCurrentColor(col, row), getCurrentColor(col+1, row), getCurrentColor(col+2, row), getCurrentColor(col+3, row));
+      if (matched == true) {
+        return getCurrentCellPlayer(col, row)
+      } else {
+        continue;
+      }
+    }
+  }
+}
 
+function checkVerticalWin(){
+  for (var col = 0; col < 7; col++) {
+    for (var row = 0; row < 3; row++) {
+      var matched =  checkForMatch(getCurrentColor(col, row), getCurrentColor(col, row+1), getCurrentColor(col, row+2), getCurrentColor(col, row+3));
+      if (matched == true) {
+        return getCurrentCellPlayer(col, row);
+      } else {
+        continue;
+      }
+    }
+  }
+}
 
+function checkDiagonalWin(){
+  for (var col = 0; col < 7; col++) {
+    for (var row = 0; row < 6; row++) {
+      var matchedPositiveSlope = checkForMatch(getCurrentColor(col, row), getCurrentColor(col+1, row+1), getCurrentColor(col+2, row+2), getCurrentColor(col+3, row+3));
+      var matchedNegativeSlope = checkForMatch(getCurrentColor(col, row), getCurrentColor(col+1, row-1), getCurrentColor(col+2, row-2), getCurrentColor(col+3, row-3));
+      if (matchedPositiveSlope == true) {
+        return getCurrentCellPlayer(col, row);
+      } else if (matchedNegativeSlope == true) {
+        return getCurrentCellPlayer(col, row);
+      } else {
+        continue;
+      }
+    }
+  }
+}
 
+function checkForMatch(firstCell,secondCell,thirdCell,fourthCell){
+  return (firstCell === secondCell && firstCell === thirdCell && firstCell === fourthCell && firstCell !== defaultCellColor);
+}
 
 function getCurrentCellPlayer(colPosition,rowPosition){
   var currentColor = getCurrentColor(colPosition,rowPosition);
@@ -112,36 +179,8 @@ function getCurrentCellPlayer(colPosition,rowPosition){
   } else if (currentColor == playerTwoColor) {
       return playerTwo;
   } else {
-      return null;
+      return "empty";
   }
-}
-
-
-
-
-
-
-
-
-function checkHorizontalWin(){
-
-}
-
-
-
-function checkVerticalWin(){
-
-}
-
-
-
-function checkDiagonalWin(){
-
-}
-
-
-function checkCellsEqual(){
-
 }
 
 
